efc31296622b6e40d414d0e635983a94b7aab02d
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2023-10-30 23:03:52 +0900

    create lambda func

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const handleCallLambda = () => {
+    axios
+      .get(
+        "https://x7426litg1.execute-api.ap-northeast-1.amazonaws.com/get_weather_data"
+      )
+      .then((response) => {
+        console.log(`response = ${JSON.stringify(response)}`);
+      })
+      .catch((error) => {
+        console.log(`error = ${error}`);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Call Lambda!</h2>
+      <button onClick={handleCallLambda}>Call Lambda func</button>
     </div>
   );
 }
