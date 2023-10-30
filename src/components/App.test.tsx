6951602644e33da35572a16b8ba19dde11467f38
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2023-10-31 00:56:37 +0900

    create simple test case

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,9 +1,35 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import axios from "axios";
 import App from "./App";
 
-test("renders learn react link", () => {
+jest.mock("axios");
+
+test("画面描写テスト", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // h2タグの文言が存在するかテスト
+  const btnElement = screen.getByText("Call Lambda!");
+  expect(btnElement).toBeInTheDocument();
+});
+
+test("API接続のテスト", async () => {
+  render(<App />);
+  const responseMock = {
+    data: {
+      city: "tokyo",
+      key: "XXX",
+    },
+    status: 200,
+  };
+  const res = (axios.get as any).mockResolvedValue(responseMock);
+  console.log(`res = ${res}`);
+
+  const button = screen.getByText("Call Lambda func");
+  fireEvent.click(button);
+
+  await waitFor(() => {
+    // 正しい引数を与えられて呼び出されたかどうかを確認
+    expect(axios.get).toHaveBeenCalledWith("https:~");
+  });
+  expect(responseMock.status).toBe(200);
 });
